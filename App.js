ba7a27bb46269cb3505ef7f359868dff03c5bc57
wdevrachit <109643623+wdevrachit@users.noreply.github.com>
2022-08-08 12:24:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Ticket from './Tickets/Ticket';
+import './Tickets/Ticket.css'
+function App() {
+  return (
+    <div className="App">
+      <Ticket></Ticket>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
